038d5df75408e5f93f3fc9c40013333b07adba65
Maria <mariafacchin00@hotmail.com>
2024-12-20 14:30:25 +0100

    Prepare the project to deploy

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    if request.method == 'GET':
+        return HttpResponse(request.method)
+    else:
+        return HttpResponse("You must have POSTed something")
--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v8bmglrm5p@mskon57mggt&jrvf7!hw7sy2tpqx9jbr9lvt$v_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
